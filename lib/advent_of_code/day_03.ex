55cdb2e43b005cb8aca304c2df984d650b920d20
Scott Kaye <ScottKaye@users.noreply.github.com>
2023-12-02 13:40:04 -0700

    Add 2022 day 3 just for posterity

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,37 @@
 defmodule AdventOfCode.Day03 do
+  defp split_into_compartments(line) do
+    half = String.length(line) |> div(2)
+    line |> to_charlist() |> Enum.chunk_every(half)
+  end
+
+  defp find_common_item([a, b]) do
+    Enum.reduce(a, [], fn i, acc ->
+      if Enum.member?(b, i) do
+        [i | acc]
+      else
+        acc
+      end
+    end)
+    |> Enum.uniq()
+  end
+
+  defp assign_priority(char) do
+    Enum.at(char, 0) -
+      if char >= ~c"a" and char <= ~c"z" do
+        96
+      else
+        38
+      end
+  end
+
   def part1(_args) do
+    File.read!("./rucksacks")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&split_into_compartments/1)
+    |> Enum.map(&find_common_item/1)
+    |> Enum.map(&assign_priority/1)
+    |> Enum.sum()
+    |> dbg
   end
 
   def part2(_args) do
